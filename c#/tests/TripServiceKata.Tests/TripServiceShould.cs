cb051d388a00bb51eba663ab0e5a3c3a766eeb33
Claudio López Ardura <cllopez@aidacanarias.com>
2022-01-11 17:18:46 +0100

    🧪 Added test for cases where the searched user doesn't have friends

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,4 +1,5 @@
-﻿using Moq;
+﻿using System.Collections.Generic;
+using Moq;
 using TripServiceKata.Entity;
 using TripServiceKata.Exception;
 using TripServiceKata.Service;
@@ -13,12 +14,25 @@ namespace TripServiceKata.Tests
         public void throw_UserNotLoggedInException_when_the_user_is_not_logged_in()
         {
             UserSessionManager userSessionManager = Mock.Of<UserSessionManager>();
+            TripService tripService = new TripService(userSessionManager);
             User nullUser = null;
             Mock.Get(userSessionManager).Setup(usm => usm.GetLoggedUser()).Returns(nullUser);
 
-            TripService tripService = new TripService(userSessionManager);
-
             Assert.Throws<UserNotLoggedInException>(() => tripService.GetTripsByUser(null));
+        }
+
+        [Fact]
+        public void returns_empty_list_when_the_user_doesnt_have_friends()
+        {
+            UserSessionManager userSessionManager = Mock.Of<UserSessionManager>();
+            TripService tripService = new TripService(userSessionManager);
+            User userWithoutFriends = new User();
+            User loggedUser = new User();
+            Mock.Get(userSessionManager).Setup(usm => usm.GetLoggedUser()).Returns(loggedUser);
+
+            List<Trip> trips = tripService.GetTripsByUser(userWithoutFriends);
+
+            Assert.Empty(trips);
         }
     }
 }
