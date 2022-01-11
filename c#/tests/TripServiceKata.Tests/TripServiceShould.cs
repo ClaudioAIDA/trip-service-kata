350250de252c407bd2847cc92fdb3a3dc776dd8d
Claudio López Ardura <cllopez@aidacanarias.com>
2022-01-11 16:33:38 +0100

    🧪 Added test for not logged users

--- c#/tests/TripServiceKata.Tests/TripServiceShould.cs
+++ c#/tests/TripServiceKata.Tests/TripServiceShould.cs
@@ -1,6 +1,17 @@
-﻿namespace TripServiceKata.Tests
+﻿using TripServiceKata.Exception;
+using Xunit;
+
+namespace TripServiceKata.Tests
 {
     public class TripServiceShould
     {
+
+        [Fact]
+        public void throw_UserNotLoggedInException_when_the_user_is_null()
+        {
+            TripService tripService = new TripService();
+
+            Assert.Throws<UserNotLoggedInException>(() => tripService.GetTripsByUser(null));
+        }
     }
 }
